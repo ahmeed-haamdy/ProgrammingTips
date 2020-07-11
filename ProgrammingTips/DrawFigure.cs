eaf75afebc6fd09d1cfdd07d3ac5e6ad2f8dc7d5
Ahmed Hamdy <ahamdy@integrant.com>
2020-07-11 04:51:30 +0200

    Initial Commit

---
+++ ProgrammingTips/DrawFigure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingTips
+{
+    public class Snippets
+    {
+        public Snippets()
+        {
+
+        }
+
+        public void DrawFigure()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("-");
+                }
+                for (int j = 0; j < 5 - i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public List<int> Fibonanci()
+        {
+            List<int> lst = new List<int>();
+
+
+
+            return lst;
+        }
+    }
+}
---
+++ ProgrammingTips/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ProgrammingTips
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //for (var i = 0; i < 1000; i++)
+            //{
+              //  Console.WriteLine(Convert.ToString(i, 2));
+            //}
+
+            var x = 10 + "5";
+
+            Parent obj = new Child();
+
+            Console.WriteLine(obj.GetName());
+
+            //Snippets snippets = new Snippets();
+            //snippets.DrawFigure();
+
+        }
+
+        public class Parent
+        {
+            public string GetName()
+            {
+                return "I am Parent";
+            }
+        }
+
+        public class Child:Parent
+        {
+            public string GetName()
+            {
+                return "I am Child";
+            }
+        }
+    }
+}
